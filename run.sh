751e6854af1fd51b05c572ad2846270c1391dfd4
Philipp <26636274+PhilippFr@users.noreply.github.com>
2025-10-17 19:18:55 +0200

    adapt run script for systemd service

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -x
 set -e
 
-PWD=`pwd`
-echo $PWD
-activate () {
-    . $PWD/venv/bin/activate
-}
+source /mnt/data/scripts/mememachineLight/venv/bin/activate
 
-activate
-python3 ./mememachine.py
-deactivate
+exec python /mnt/data/scripts/mememachineLight/mememachine.py
